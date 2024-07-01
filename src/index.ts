8aee823c43b5aa47d8c8fa22a6b8ac1417fd6e2e
moises-herrera <moisesherrera037@gmail.com>
2024-07-01 18:03:12 -0500

    feat: add scraper function

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { chromium } from 'playwright';
+import { LABELS } from './constants/labels';
+import { Price } from './types/price';
+import { Category } from './types/category';
+
+const BASE_URL = 'https://es.investing.com';
+
+/**
+ * Get the price of an asset.
+ *
+ * @param category The category of the asset.
+ * @param pair The pair of the asset.
+ * @returns The price of the asset.
+ */
+const getPrice = async (
+  category: Category,
+  pair: string
+): Promise<Price | undefined> => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(`${BASE_URL}/${category}/${pair}`);
+
+  try {
+    const promises: Promise<{
+      [key: keyof typeof LABELS]: Price[keyof Price] | null;
+    }>[] = Object.entries(LABELS).map(async ([key, value]) => {
+      return page.textContent(value).then((text) => {
+        const weekRange = key === 'weekRange' ? text?.split('-') : null;
+
+        if (weekRange?.length) {
+          return {
+            [key]: {
+              low: parseFloat(weekRange[0].replace(',', '.')),
+              high: parseFloat(weekRange[1].replace(',', '.')),
+            },
+          };
+        }
+
+        return {
+          [key]: text ? parseFloat(text.replace(',', '.')) : null,
+        };
+      });
+    });
+
+    const results = await Promise.allSettled(promises);
+    const data = results.reduce((acc, result) => {
+      const value = 'value' in result ? result.value : null;
+      return { ...acc, ...value };
+    }, {} as Price);
+
+    return data;
+  } catch (error: unknown) {
+    console.error(error);
+  } finally {
+    await browser.close();
+  }
+};
+
+const stockMarketScraper = {
+  getPrice,
+};
+
+export default stockMarketScraper;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
 import { describe, expect, test } from '@jest/globals';
-import { getPrice } from '../src/index';
+import stockMarketScraper from '../src/index';
 import { Category } from '../src/types/category';
 
 describe('getPrice', () => {
   test('should return the price of an asset', async () => {
     const category: Category = 'currencies';
     const pair = 'eur-usd';
-    const price = await getPrice(category, pair);
+    const price = await stockMarketScraper.getPrice(category, pair);
     expect(price).toBeDefined();
     expect(price?.ask).toBeGreaterThan(0);
     expect(price?.buy).toBeGreaterThan(0);
